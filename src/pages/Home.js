3bceaff46801e32bfeab805b3c1119a94867ea28
cesar <csgalindos@hotmail.com>
2020-05-11 21:53:30 -0500

    [userD] add login, user, exam initial

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,19 @@ import {Link} from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div className="home">
-      <Link to="/login">Login</Link>
+    <div className='home'>
+      <button>
+        <Link to='/login'>Login</Link>
+      </button>
+      <button>
+        <Link to='/users'>users</Link>
+      </button>
+      <button>
+        <Link to='/user'>user</Link>
+      </button>
+      <button>
+        <Link to='/exams'>exam</Link>
+      </button>
     </div>
   )
 }
