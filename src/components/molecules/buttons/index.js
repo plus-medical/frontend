58831d859c4254f59591c7b36e705f4ab3f6ba01
Iván Darío Sánchez <cosmosoft.soluciones@gmail.com>
2020-05-14 12:00:57 -0500

    [button] fix button typo (#35)

--- src/components/molecules/buttons/index.js
+++ src/components/molecules/buttons/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './styles.scss'
 
-
 export function ButtonPrincipal ({ text }) {
   return (
     <button className='button_principal'>
       {text}
-    )
+    </button>
+  )
 }
 
 export function CircleButton () {
