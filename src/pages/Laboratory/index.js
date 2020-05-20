3ae1349b0fff9db9ccb6f3ef068858a3c48af849
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-05-20 02:15:50 -0500

    [exam laboratory] implement placeholder floating label in exam and laboratory

--- src/pages/Laboratory/index.js
+++ src/pages/Laboratory/index.js
@@ -3,19 +3,78 @@ import { Link } from 'react-router-dom'
 
 export function Laboratory () {
   return (
-
     <form>
       <div className='form-section'>
         <div className='form-section__title'>Crear laboratorio</div>
-        <input className='form-input' type='text' placeholder='Laborotario' />
-        <input className='form-input' type='text' placeholder='NIT' />
-        <input className='form-input' type='text' placeholder='Dirección' />
-        <input className='form-input' type='text' placeholder='Email' />
-        <input className='form-input' type='text' placeholder='Teléfono' />
-        <select className='form-select' name='status'>
-          <option value='1'>Activo</option>
-          <option value='2'>Inactivo</option>
-        </select>
+        <div className='form-group'>
+          <input
+            id='laboratory-name'
+            className='form-input'
+            type='text'
+            aria-label='Nombre del laborotario'
+            autocomplete='off'
+            required
+          />
+          <label className='form-placeholder' htmlFor='laboratory-name'>Laboratorio</label>
+        </div>
+        <div className='form-group'>
+          <input
+            id='laboratory-tin'
+            className='form-input'
+            type='text'
+            aria-label='nit'
+            autocomplete='off'
+            required
+          />
+          <label className='form-placeholder' htmlFor='laboratory-tin'>NIT</label>
+        </div>
+        <div className='form-group'>
+          <input
+            id='laboratory-address'
+            className='form-input'
+            type='text'
+            aria-label='Dirección'
+            autocomplete='off'
+            required
+          />
+          <label className='form-placeholder' htmlFor='laboratory-address'>Dirección</label>
+        </div>
+        <div className='form-group'>
+          <input
+            id='laboratory-email'
+            className='form-input'
+            type='text'
+            aria-label='Correo electrónico'
+            autocomplete='off'
+            required
+          />
+          <label className='form-placeholder' htmlFor='laboratory-email'>Correo electrónico</label>
+        </div>
+        <div className='form-group'>
+          <input
+            id='laboratory-phone'
+            className='form-input'
+            type='text'
+            aria-label='Teléfono'
+            autocomplete='off'
+            required
+          />
+          <label className='form-placeholder' htmlFor='laboratory-phone'>Teléfono</label>
+        </div>
+        <div className='form-group'>
+          <select
+            id='laboratory-status'
+            className='form-select'
+            aria-label='Estado del laboratorio'
+            autocomplete='off'
+            required
+          >
+            <option />
+            <option value='1'>Activo</option>
+            <option value='2'>Inactivo</option>
+          </select>
+          <label className='form-placeholder' htmlFor='laboratory-status'>Estado</label>
+        </div>
         <div className='form-section__buttons'>
           <Link to='/dashboard'>
             <button className='form-button secondary'>Cancelar </button>
