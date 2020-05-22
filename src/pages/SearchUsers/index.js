a95c245056a0b33b3b7b531e1564fbe27e277304
cesar <csgalindos@hotmail.com>
2020-05-22 17:47:04 -0500

    [ReviewForm] updated search page

--- src/pages/SearchUsers/index.js
+++ src/pages/SearchUsers/index.js
@@ -2,19 +2,27 @@ import React from 'react'
 import './style.scss'
 import { Item } from '../../components/Item/index'
 import { CircleButton } from '../../components/buttons/index'
+import { useForm } from 'react-hook-form'
 
 export function Users () {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => console.log(data)
   const link = '/user'
   return (
     <section className='users'>
-      <div className='users__container'>
+      <form
+        className='users__container'
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <input
           type='search'
           placeholder='buscar usuario'
           aria-label='busqueda de usuarios'
           className='users__input'
+          name='search'
+          ref={register}
         />
-      </div>
+      </form>
       <ul className='users__list'>
         <Item data={link} />
         <Item data={link} />
