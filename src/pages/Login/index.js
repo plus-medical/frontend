f8eeb05e35ce24551cfd0fec078ee6f1388d82b8
cesar <csgalindos@hotmail.com>
2020-05-24 01:29:03 -0500

    [homepage] updated login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -51,38 +51,40 @@ export function Login () {
   ) : (
     <section className='login'>
       <img src={Logo} alt='Plus Medical Logo' />
-      <form className='login__form' onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className='login__input login__input-user'
-          name='username'
-          type='tel'
-          placeholder='Numero de Identificacion'
-          aria-label='Numero de identicacion'
-          ref={register({ required: true })}
-        />
-        {errors.username && (
-          <span className='login__error'>Campo requerido</span>
-        )}
-        <input
-          className='login__input login__input-psw'
-          name='password'
-          type='password'
-          placeholder='Contraseña'
-          aria-label='Contraseña'
-          ref={register({ required: true })}
-        />
-        {errors.password && (
-          <span className='login__error'>Campo requerido</span>
-        )}
+      <div className='login-container'>
+        <form className='login__form' onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className='login__input login__input-user'
+            name='username'
+            type='tel'
+            placeholder='Numero de Identificacion'
+            aria-label='Numero de identicacion'
+            ref={register({ required: true })}
+          />
+          {errors.username && (
+            <span className='login__error'>Campo requerido</span>
+          )}
+          <input
+            className='login__input login__input-psw'
+            name='password'
+            type='password'
+            placeholder='Contraseña'
+            aria-label='Contraseña'
+            ref={register({ required: true })}
+          />
+          {errors.password && (
+            <span className='login__error'>Campo requerido</span>
+          )}
 
-        <button
-          aria-label='button iniciar session'
-          className='login__button'
-          type='submit'
-        >
-          Iniciar sesión
-        </button>
-      </form>
+          <button
+            aria-label='button iniciar session'
+            className='login__button'
+            type='submit'
+          >
+            Iniciar sesión
+          </button>
+        </form>
+      </div>
       <Link className='login__link' to='/'>
         Olvidó su contraseña
       </Link>
