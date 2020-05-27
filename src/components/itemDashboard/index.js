993fe9b191b3d3f6f3f39316cd53905efc70514b
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-05-26 23:13:19 -0500

    [dashboard patient] Dashboard configuration according to role

---
+++ src/components/itemDashboard/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaUsers, FaFileMedicalAlt, FaFlask, FaGreaterThan, FaBookMedical } from 'react-icons/fa'
+
+export const ItemDashboard = ({ icon, path, itemText }) => {
+  const selectIcon = () => {
+    switch (icon) {
+      case 'user':
+        return <FaUsers />
+      case 'exam':
+        return <FaFileMedicalAlt />
+      case 'laboratory':
+        return <FaFlask />
+      case 'consultation':
+        return <FaBookMedical />
+    }
+  }
+  return (
+    <Link to={path}>
+      <div className='dashboard-item'>
+        <div className='dashboard-item__img'>
+          {selectIcon()}
+        </div>
+        <div className='dashboard-item__txt'>
+          {itemText}
+        </div>
+        <div className='dashboard-item__togo'>
+          <FaGreaterThan />
+        </div>
+      </div>
+    </Link>
+  )
+}
