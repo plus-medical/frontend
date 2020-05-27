9df514295583a5e89c1e65432e21d3de150fc3ec
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-05-27 02:47:36 -0500

    [upload-users] page creation for massive user load

---
+++ src/components/uploadFile/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { FaCloudUploadAlt } from 'react-icons/fa'
+import './styles.scss'
+
+export const UploadFile = ({ accept }) => {
+  const [fileName, setFileName] = useState('Cargar Archivo')
+
+  const handleFileUpload = () => {
+    const fileNameSelected = document.getElementById('upload-file__input').value
+    const fileNameResult = fileNameSelected.split('\\').pop().split('/').pop()
+    setFileName(fileNameResult)
+  }
+  return (
+    <div>
+      <label className='upload-file__label'>
+        <FaCloudUploadAlt className='upload-file__icon' />
+        <span id='upload-file__file-selected' />
+        <input
+          id='upload-file__input'
+          name='fileName'
+          type='file'
+          className='upload-file__input'
+          onChange={handleFileUpload}
+          accept={accept}
+        />
+        {fileName}
+      </label>
+    </div>
+  )
+}
---
+++ src/pages/UploadUsers/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { UploadFile } from '../../components/uploadFile'
+import './styles.scss'
+
+export const UploadUsers = () => {
+  return (
+    <section className='upload-users'>
+      <div className='upload-users__title'>Crear Usuarios</div>
+      <UploadFile accept='.csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel' />
+      <div className='upload-users__buttons'>
+        <Link to='/users'>
+          <button className='upload-users__btn-secondary'>Cancelar </button>
+        </Link>
+        <button type='submit' className='upload-users__btn-primary'>Guardar</button>
+      </div>
+    </section>
+  )
+}
