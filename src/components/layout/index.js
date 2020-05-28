f730a36c8e0826fbeefa75599569180b391aac0d
cesar <csgalindos@hotmail.com>
2020-05-28 02:14:32 -0500

    [test] add Header

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './styles.scss'
 import FooterMobile from '../footer/index'
-import { HeaderMobile } from '../header'
+import HeaderMobile from '../header/index'
 
 export default function LayoutMobile ({ children }) {
   return (
